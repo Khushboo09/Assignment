fd0459779ff895e2dfb71e45547d34ae767cdd14
Khushboo <khushboo.kochhar@nagarro.com>
2019-06-05 14:05:20 +0530

    Removed pod folder

--- Pods/Target Support Files/Pods-DOOR~DELIVER/Pods-DOOR~DELIVER-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DOOR_DELIVER : NSObject
-@end
-@implementation PodsDummy_Pods_DOOR_DELIVER
-@end
--- Pods/Target Support Files/Pods-DOOR~DELIVERUITests/Pods-DOOR~DELIVERUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DOOR_DELIVERUITests : NSObject
-@end
-@implementation PodsDummy_Pods_DOOR_DELIVERUITests
-@end
